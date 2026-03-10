7c5d87d90b1106b23ee300ccf4eb1517289d34e2
kevin kiplangat <kiplangatkevin335@gmail.com>
2026-03-10 10:24:20 +0300

    customized for maximum search result

--- search.py
+++ search.py
@@ -12,35 +12,32 @@ with open("data/documents.json", 'r', encoding='utf-8') as f:
 def search(query, top_k):
     query_embedding = model.encode([query], convert_to_numpy=True)
     similarities = np.dot( embeddings, query_embedding.T).flatten()
-    top_indices = np.argsort()
-# if __name__ == "__main__":
-#     while True:
-#         query = input("\nEnter search query (or 'quit' to exit): ")
-#         if query.lower() == 'quit':
-#             break
-        
-#         results = search(query)
-#         print(f"\nTop {len(results)} results:")
-#         for i, result in enumerate(results, 1):
-#             print(f"\n{i}. {result['filename']} (Score: {result['score']:.4f})")
-#             print(f"   {result['content']}")
+    top_indices = np.argsort(similarities)[::-1][:top_k]
+    
+    results = []
+    for idx in top_indices:
+        results.append({
+            "filename": documents[idx]["filename"],
+            "score": float(similarities[idx]),
+            "content": documents[idx]["content"][:200] + "..."
+        })
+    return results
 
 
-
+if __name__ == "__main__":
+    while True:
+        query = input("\nEnter search query (or 'quit' to exit): ") # the users input
+        if query.lower() == 'quit':  # allow the user to quit
+            break 
+        top_k = input("\nEnter the maximum number of search result you want to see")
+        results = search(query, top_k) 
+        print(f"\nTop {len(results)} results:")
+        for i, result in enumerate(results, 1):
+            print(f"\n{i}. {result['filename']} (Score: {result['score']:.4f})")
+            print(f"   {result['content']}")
+    
+    
 
 
 
    
-# def search(query, top_k=3):
-#     query_embedding = model.encode([query], convert_to_numpy=True)
-#     similarities = np.dot(embeddings, query_embedding.T).flatten()
-#     top_indices = np.argsort(similarities)[::-1][:top_k]
-    
-#     results = []
-#     for idx in top_indices:
-#         results.append({
-#             "filename": documents[idx]["filename"],
-#             "score": float(similarities[idx]),
-#             "content": documents[idx]["content"][:200] + "..."
-#         })
-#     return results
